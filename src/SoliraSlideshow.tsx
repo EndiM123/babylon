9f5620b36c1ef0c0bf8c2fb5e9d303edb5f0bbe3
Endi Mullaademi <endimullaademi@Mac.lan>
2025-05-03 18:19:07 +0200

    fix: use lowercase filenames for Solira slideshow images

--- src/SoliraSlideshow.tsx
+++ src/SoliraSlideshow.tsx
@@ -10,9 +10,9 @@ const SLIDES = [
     type: 'image/png',
   },
   {
-    src: '/solira5.png',
+    src: '/solira2.png',
     alt: 'Solira Summer Collection - Look 2',
-    srcSet: '/solira5.png 800w, /solira5.png 1600w, /solira5.png 2400w',
+    srcSet: '/solira2.png 800w, /solira2.png 1600w, /solira2.png 2400w',
     type: 'image/png',
   },
   {
